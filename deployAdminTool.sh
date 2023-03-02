082410c244fca761eac7f1eb3431ca47391452b8
Shin Imai <binaryninja0101@yahoo.com>
2023-03-02 12:05:56 +0000

    Updated

--- deployAdminTool.sh
+++ deployAdminTool.sh
@@ -1,3 +1,25 @@
+#!/bin/bash
+
+
+BASEDIR=${0%/*}
+
+if [[ ${0} == ${BASEDIR} ]];then
+  BASEDIR="."
+fi
+
+NODE=$1
+
+if [ -z ${NODE} ];then
+  echo "NODE isn't specified"
+  exit 1
+fi
+
+NUM=$(awk '/^__YAML__/ {print NR}' $0)
+
+sed -n "$((NUM+1)),\$p" $0 | sed 's#__NODE_NAME__#'${NODE}'#' | kubectl apply -f -
+exit 0
+
+__YAML__
 apiVersion: v1
 kind: Pod
 metadata:
